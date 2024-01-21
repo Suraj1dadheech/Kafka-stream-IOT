674ee1f21b70cf735cfcde52d6050360e00d6a69
costrategix <naveen.shreenatha@costrategix.com>
2024-01-21 23:31:49 +0530

    Aggregation APIs

--- PDP-kafka-project/MongoApp/src/main/java/com/pdp/MongoApp/controller/IotController.java
+++ PDP-kafka-project/MongoApp/src/main/java/com/pdp/MongoApp/controller/IotController.java
@@ -3,17 +3,20 @@ package com.pdp.MongoApp.controller;
 
 import com.pdp.MongoApp.collection.Health;
 import com.pdp.MongoApp.service.IotService;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 public class IotController {
-
 
     @Autowired
     private IotService iotService;
@@ -26,6 +29,19 @@ public class IotController {
         return iotService.getHealthByStepsAndHeartRate(steps,heartRate);
     }
 
+
+    @GetMapping("/basic/aggregation")
+    public ResponseEntity<List<Document>> getMaxTempratureOfEachDevice(){
+        return iotService.getMaxTemperatureOfEveryDevice();
+    }
+
+    @GetMapping("/basic/aggregation/calorie")
+    public ResponseEntity<List<Document>> getAvgCalorieBurnt(
+            @RequestParam("startTimestamp") String startDateTime,
+            @RequestParam("endTimestamp") String endDateTime){
+
+        return iotService.getMaximumCaloriesBurned(startDateTime,endDateTime);
+    }
 
 
 }
--- PDP-kafka-project/MongoApp/src/main/java/com/pdp/MongoApp/repository/HealthRepository.java
+++ PDP-kafka-project/MongoApp/src/main/java/com/pdp/MongoApp/repository/HealthRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface HealthRepository extends MongoRepository<Health,String> {
-
+    //rawQuery = db.health.find({stepsTaken:{$gte:6000},heartRate:{$lt:80}})
     @Query(value="{stepsTaken:{$gte:?0},heartRate:{$lt:?1}}",fields="{sleepDuration:0 , spo2:0}")
     public List<Health> findByStepsAndHeartRate(Integer stepsTaken, Integer heartRate);
 }
