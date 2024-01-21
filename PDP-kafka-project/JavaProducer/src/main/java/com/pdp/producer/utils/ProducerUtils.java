2655435dea221b4da26820a4ec77286b048df012
praveen <praveen.kumar@costrategix.com>
2024-01-21 10:23:06 +0530

    Added IOT devices

--- PDP-kafka-project/JavaProducer/src/main/java/com/pdp/producer/utils/ProducerUtils.java
+++ PDP-kafka-project/JavaProducer/src/main/java/com/pdp/producer/utils/ProducerUtils.java
@@ -13,7 +13,8 @@ public class ProducerUtils {
                 "34053e20-1e9b-4163-a476-b5c26a3bc98c",
                 "3720970a-84df-41d4-9a22-215af0370a46",
                 "6a6c2daa-24f9-4251-aede-ea6e5b96ce1f",
-                "67e4bb85-2c62-4869-b7e8-40a50c26f715"};
+                "67e4bb85-2c62-4869-b7e8-40a50c26f715",
+                ""};
         return uuids[random.nextInt(uuids.length)];
     }
 
@@ -23,7 +24,8 @@ public class ProducerUtils {
                 "f41e8631-cac4-4ab2-817e-1ae5f40cc9bc",
                 "da4fe9ff-0fba-44c4-887e-152a5242aa65",
                 "1ef81f9a-0ee8-49c1-927e-18ac445d45f8",
-                "f0048997-5110-4f82-b979-ba95c608259c"};
+                "f0048997-5110-4f82-b979-ba95c608259c",
+                ""};
         return uuids[random.nextInt(uuids.length)];
 
     }
